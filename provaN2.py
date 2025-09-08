79cc37fc3b9f97da39f5119006c22cac146ec9d9
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2025-09-08 16:50:38 -0400

    tentativa fracassada

--- provaN2.py
+++ provaN2.py
@@ -53,25 +53,84 @@
 # ter sido escrita no alfabeto alienígena, imprima a letra ‘S’ maiúscula; caso contrário, imprima a
 # letra ‘N’ maiúscula.
 
-K, N = input("").split(" ")
-K = int(K)
-N = int(N)
+# K, N = input("").split(" ")
+# K = int(K)
+# N = int(N)
 
 
-quantidadeChar = input("")
-mensagemEnviada = input("")
+# quantidadeChar = input("")
+# mensagemEnviada = input("")
 
-letrasAlfabeto = list(quantidadeChar)
-letrasMensagem = list (mensagemEnviada)
+# letrasAlfabeto = list(quantidadeChar)
+# letrasMensagem = list (mensagemEnviada)
 
-pode = True
-for letras in mensagemEnviada:
-    if letras not in quantidadeChar:
-        pode = False
-        break
+# pode = True
+# for letras in mensagemEnviada:
+#     if letras not in quantidadeChar:
+#         pode = False
+#         break
 
-if pode == True:
-    print("S")
-else: 
-    print("N")
+# if pode == True:
+#     print("S")
+# else: 
+#     print("N")
+
+
+# Sua tarefa é: dadas as dimensões N e M da pista de dança, a quantidade P de passos da dança e
+# a ordem dada pelo professor a cada passo, determine qual aluno estará em cada quadrado da pista
+# ao fim da dança.
+# Entrada
+# A primeira linha da entrada é composta por três inteiros N, M e P indicando, respectivamente, o
+# número de linhas da pista de dança, o número de colunas da pista de dança, e o número de passos
+# da dança.
+# As próximas P linhas descrevem as ordens dadas pelo professor. A i-ésima dessas linhas contém
+# uma letra maiúscula Oi
+# , que pode ser ‘L’ ou ‘C’, seguida de dois inteiros distintos Ai e Bi
+# .
+# • Se Oi = ‘L’, o professor ordenou a troca das linhas Ai e Bi
+# .
+# • Se Oi = ‘C’, o professor ordenou a troca das colunas Ai e Bi
+# .
+
+
+# N,M,
+P = 3 #input("").split()
+N = 4 #int(N)
+M = 3 #int(M)
+P = int(P)
+
+#inline
+Matriz = []
+for i in range(N):
+    Matriz.append([int(M*i+x+1) for x in range(M)])
+# print(Matriz)
+
+#cria uma varivel que começa no 1
+# cres = 1
+# #cria um for que percore de i até N
+# #dentro tem um j que vai ate M
+# #dentro desse segundo for pega as posições i e j da matriz e atribui o valor da variavel cres para cada um
+# #depois acrescenta +1 em cada posição
+# for i in range(N):
+#     for j in range(M):
+#         Matriz[i][j] = cres
+#         cres = cres + 1
+#print(Matriz)
+i = 0
+auxiliar = 0
+while (i < P):
+    comandoDanca,linha,coluna = input("De um comando L ou C seguido das linhas e colunas: ").split()
+    linha = int(linha)
+    coluna = int(coluna)
+    i+= 1
+    if comandoDanca == "C":
+     for i in range(coluna):
+        
+
+
+
+
+
+
+
 
