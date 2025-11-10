afd03fd1248ee5c05b888675466068ce4daef064
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2025-11-10 16:30:56 -0400

    a

--- matriz.py
+++ matriz.py
@@ -1,64 +1,23 @@
-# matriz = [
-#     [1,"l",3],
-#     [4,5,"b"],
-#     ["rodolfo",2,45 ]
-#     ]
+# n, m = map(int, input().split()) 
+# matriz = []
 
-# matriz[0].pop(0)
-
+# for i in range(n):
+#     line = [int (x) for x in input().split()]
+#     matriz.append(line)
 # print(matriz)
 
-# matriz[0].insert(0, 1)
+# i = 0
+# j = 0
+# k = 0
+# while k<n:
+#     numerosInseridos = list(map(int, input().split()))
+#     matriz.append(numerosInseridos)
+#     k+=1
+#     # print(matriz)
 
-# print(matriz)
-
-
-# nLC, qSimu = map(int, input().split())
-
-# Matriz = []
-
-# for _ in range(nLC):
-#     linha_str = input()
-#     Matriz.append([int(letra) for letra in linha_str])
-# for _ in range(qSimu):
-#     proximo_tabuleiro = [[0 for _ in range (nLC)] for _ in range(nLC)]
-
-#     for Lin in range(nLC):
-#         for Col in range(nLC):
-#             vizinhos_Vivos = 0
-#             for dLin in [-1,0,1]:
-#                 for dCol in [-1,0,1]:
-#                     if dLin == 0 and dCol == 0:
-#                         continue
-#                         viz_li, viz_col = Lin + dLin, Col + dCol
-#                         if 0 <= viz_li < nLC and 0 <= viz_col < nLC:
-#                             vizinhos_Vivos += Matriz[viz_li],[viz_col]
-        
-#             celula_atual_viva = Matriz[Lin][Col] == 1
-
-#             if vizinhos_Vivos == 3 or (celula_atual_viva and vizinhos_Vivos == 2):
-#                 proximo_tabuleiro[Lin][Col] = 1
-#     Matriz = proximo_tabuleiro
-#     for Lin in range(nLC):
-#      print("".join(map(str, Matriz[Lin])))
-
-
-
-
-n, m = map(int, input().split()) 
-matriz = []
-i = 0
-j = 0
-k = 0
-while k<n:
-    numerosInseridos = list(map(int, input().split()))
-    matriz.append(numerosInseridos)
-    k+=1
-    # print(matriz)
-
-for i in range (len(matriz[j])):
-        for j in range(len(matriz)):
-            print(matriz[i][j], end="\n")
+# for i in range (len(matriz[j])):
+#         for j in range(len(matriz)):
+#             print(matriz[i][j], end="\n")
 
 
 # parte inutil
@@ -75,17 +34,89 @@ for i in range (len(matriz[j])):
 
 
 #===========================
+
 #Matriz linha
 
-if len(matriz) > 1:
-    print("não é linha")
-else:
-    print("é matriz linha")
+# if len(matriz) > 1:
+#     print("não é linha")
+# else:
+#     print("é matriz linha")
 
 
-if m > 1:
-    print("Não é uma matriz coluna")
-else:
-    print("É matriz coluna")
+# if m > 1:
+#     print("Não é uma matriz coluna")
+# else:
+#     print("É matriz coluna")
 
 #============================
+
+# n, m = map(int, input().split()) 
+# matriz = []
+
+# # for i in range(n):
+# #     line = [int (x) for x in input().split()]
+# #     matriz.append(line)
+# print(matriz)
+
+# if(n == m):
+#     ehDiagonal = True
+#     for i in range(n):
+#         for j in range(m):
+#             if(i!=j):
+#                 if(matriz[i][j]!=0):
+#                     ehDiagonal = False
+#     if (ehDiagonal):
+#         print("é Diagonal")
+#     else:
+#         print("não é Diagonal")
+
+
+
+# ehNula =True
+# for i in range(n):
+#     for j in range(m):
+#         if(matriz[i][j]!=0):
+#             ehNula = False
+
+# if (ehNula):
+#     print("é uma matriz nula")
+# else:
+#     print("não é uma matriz nula")
+
+
+
+
+# n, m = map(int, input().split()) 
+
+# matriz = [[0 for m in range(m)] for n in range(n)]
+
+# # print(matriz)
+
+# for i in range(n):
+#     for j in range(m):
+#         if(i>j):
+#             matriz[i][j] = 10 * i + j
+#         elif (j>i) or (j==i):
+#             matriz[i][j] = 0
+# print(matriz)
+
+n, m = map(int, input().split()) 
+matriz = [[0 for m in range(m)] for n in range(n)]
+
+# for i in range(n):
+#     line = [int (x) for x in input().split()]
+#     matriz.append(line)
+print(matriz)
+
+
+
+for i in range(n):
+    for j in range(m):
+        if i == j:
+            matriz[i][j] = 1
+        elif i+j == n-1:
+            matriz[i][j] = -1
+        elif i < j:
+            matriz[i][j] = 10*i+j
+for linha in (matriz):
+    print(*linha, sep=" ")
---
+++ matrizAulapassada.py
@@ -0,0 +1,43 @@
+# matriz = [
+#     [1,"l",3],
+#     [4,5,"b"],
+#     ["rodolfo",2,45 ]
+#     ]
+
+# matriz[0].pop(0)
+
+# print(matriz)
+
+# matriz[0].insert(0, 1)
+
+# print(matriz)
+
+
+# nLC, qSimu = map(int, input().split())
+
+# Matriz = []
+
+# for _ in range(nLC):
+#     linha_str = input()
+#     Matriz.append([int(letra) for letra in linha_str])
+# for _ in range(qSimu):
+#     proximo_tabuleiro = [[0 for _ in range (nLC)] for _ in range(nLC)]
+
+#     for Lin in range(nLC):
+#         for Col in range(nLC):
+#             vizinhos_Vivos = 0
+#             for dLin in [-1,0,1]:
+#                 for dCol in [-1,0,1]:
+#                     if dLin == 0 and dCol == 0:
+#                         continue
+#                         viz_li, viz_col = Lin + dLin, Col + dCol
+#                         if 0 <= viz_li < nLC and 0 <= viz_col < nLC:
+#                             vizinhos_Vivos += Matriz[viz_li],[viz_col]
+        
+#             celula_atual_viva = Matriz[Lin][Col] == 1
+
+#             if vizinhos_Vivos == 3 or (celula_atual_viva and vizinhos_Vivos == 2):
+#                 proximo_tabuleiro[Lin][Col] = 1
+#     Matriz = proximo_tabuleiro
+#     for Lin in range(nLC):
+#      print("".join(map(str, Matriz[Lin])))
